c46a11a37de33b4092cc96a95feb082686cc9b04
ramswaroop <ramswarooppatra@gmail.com>
2016-06-05 12:56:47 -0400

    initial commit

---
+++ src/main/java/me/ramswaroop/botkit/BotKitApplication.java
@@ -0,0 +1,12 @@
+package me.ramswaroop.botkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotKitApplication.class, args);
+	}
+}
---
+++ src/test/java/me/ramswaroop/botkit/BotKitApplicationTests.java
@@ -0,0 +1,16 @@
+package me.ramswaroop.botkit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BotKitApplication.class)
+public class BotKitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
