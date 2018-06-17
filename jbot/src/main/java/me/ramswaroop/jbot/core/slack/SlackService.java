35d618fbe3adf552b20c26e6fcffadaf511c33a4
Ram Patra <ramswarooppatra@gmail.com>
2018-06-17 17:55:55 +0100

    optimized imports + refactored code

--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackService.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackService.java
@@ -7,7 +7,6 @@ import me.ramswaroop.jbot.core.slack.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/models/RTM.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/models/RTM.java
@@ -1,7 +1,6 @@
 package me.ramswaroop.jbot.core.slack.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by ramswaroop on 12/06/2016.
